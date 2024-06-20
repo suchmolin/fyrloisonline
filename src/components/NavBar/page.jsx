1680ed22920afe6cc93a3ae94c5a18800fb58f97
programadorlerobotica <jsuchmolin.ads@gmail.com>
2024-06-20 15:59:33 -0400

    fixing warnings en consola

--- src/components/NavBar/page.jsx
+++ src/components/NavBar/page.jsx
@@ -1,18 +1,10 @@
-/* eslint-disable @next/next/no-img-element */
-
 import Image from "next/image";
 import DropDownMenu from "../DropDownMenu/page";
 
 export default function NavBar() {
   return (
     <nav className="navBar h-[70px] px-10 bg-gray-100 flex justify-between items-center relative">
-      <img
-        src="/img/1.png"
-        alt="logo"
-        width={137}
-        height={53}
-        className="hover:scale-105 transition-all duration-500 cursor-pointer"
-      />
+      <Image src="/img/1.png" alt="logo" width={137} height={53} className="" />
       <DropDownMenu />
     </nav>
   );
