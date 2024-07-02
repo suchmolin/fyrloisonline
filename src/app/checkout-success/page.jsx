b6a8dd99ab68abb64b16c0cbc2820390e305999d
programadorlerobotica <jsuchmolin.ads@gmail.com>
2024-07-02 13:08:31 -0400

    borar carrito

--- src/app/checkout-success/page.jsx
+++ src/app/checkout-success/page.jsx
@@ -1,4 +1,14 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client";
+import { useContext, useEffect } from "react";
+import { OpenModalContext } from "@/context/openModal";
 export default function Page() {
+  const { setCantCart, setCartInfo } = useContext(OpenModalContext);
+  useEffect(() => {
+    setCantCart(0);
+    setCartInfo([]);
+    localStorage.removeItem("cartInfo");
+  }, []);
   return (
     <div className="w-full flex justify-center items-center">
       <div className="w-8/12 flex flex-col items-center py-20">
