93c3b0a5450761ed49e39ad8dc6385eaeda41b79
programadorlerobotica <jsuchmolin.ads@gmail.com>
2024-07-02 14:04:07 -0400

    compra cursos individuales

--- src/app/api/checkout/route.jsx
+++ src/app/api/checkout/route.jsx
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 import { cursosInfo } from "@/cursosInfo";
-import { metadata } from "@/app/layout";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
