a432efa5eb9e8f75c571d2296beaecee0851ec2d
programadorlerobotica <jsuchmolin.ads@gmail.com>
2024-07-04 08:31:45 -0400

    added blog section

--- src/components/SeccionBlog/page.jsx
+++ src/components/SeccionBlog/page.jsx
@@ -3,7 +3,9 @@ import BlogUltimosArticulos from "../BlogUltimosArticulos/page";
 export default function SeccionBlog() {
   return (
     <div className="w-full flex flex-col items-center py-20">
-      <h2 className="text-4xl font-bold text-center pb-4 px-5">Blog</h2>
+      <h2 className="text-4xl font-bold text-center pb-4 px-5 text-[#000b7a]">
+        Blog
+      </h2>
       <p className="text-center text-gray-500 text-lg">
         Aprende acerca de temas imprescindibles para mejorar tu inglés.
       </p>
