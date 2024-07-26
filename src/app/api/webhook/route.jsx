1a9a325296edf35cd7d83438e7d0eb1e3d7e731b
josesuchmolin <suchmolin11@gmail.com>
2024-07-26 13:59:08 -0400

    estructura de correo de pago

--- src/app/api/webhook/route.jsx
+++ src/app/api/webhook/route.jsx
@@ -35,16 +35,9 @@ export async function POST(request) {
       const idTransaccion = session.payment_intent
       const fecha = JSON.parse(event.created)
       const telefono = session.custom_fields[0].numeric.value
-      console.log(
-        new Date(fecha * 1000).toLocaleDateString("es-ES", {
-          timeZone: "America/New_York",
-          hour: "2-digit",
-          minute: "2-digit",
-        })
-      )
 
       const respuesta = await resend.emails.send({
-        from: "no-reply@fyrlois.us",
+        from: "info@fyrlois.us",
         to: correoCliente,
         subject: "PAGO ONLINE Fyr Lois Online",
         html: `
@@ -61,12 +54,12 @@ export async function POST(request) {
           Numero de transaccion: ${idTransaccion}
         </h2>
 
-        <div style="display: flex; gap: 28px; padding-top: 40px; padding-bottom: 40px; justify-content: center; font-size: 12px; line-height: 16px; width: 100%;" >
-          <div>
+        <div style=" padding-top: 40px; padding-bottom: 10px; font-size: 12px; line-height: 16px; width: 100%; text-align: center;" >
+          <div style="display: inline-block; padding-right:20px;">
             <h3 style="font-weight: bold; color: rgb(107 114 128);  padding-right: 28px;" >Monto Pagado</h3>
             <p>$${total}.00</p>
           </div>
-          <div>
+          <div style="display: inline-block;">
             <h3 style="font-weight: bold; color: rgb(107 114 128);" >Fecha de pago</h3>
             <p>${fecha}</p>
           </div>
@@ -88,16 +81,16 @@ export async function POST(request) {
                   src="https://fyrlois-us.vercel.app/_next/image?url=%2Fimg%2F${curso.img}&w=1920&q=75"
                   alt="${curso.title}"
                 />
-                <div style="display: flex; justify-content: space-between; width: 350px;">
-                  <p>${curso.title} x ${producto.cantidad}</p>
-                  <p>$${curso.price}.00</p>
+                <div style="width: 350px; display:flex;">
+                  <p style="display:inline-block; margin-right: auto; margin-left: 0;">${curso.title} x ${producto.cantidad}</p>
+                  <p style="display:inline-block; margin-left: auto; margin-right: 0;">$${curso.price}.00</p>
                 </div>
               </div>
               `
           })}
           
           <hr />
-          <div style="display: flex; justify-content: space-between; padding: 12px 0; font-weight: bold; color: rgb(21 94 117); width:100%;" >
+          <div style="display:flex; padding: 12px 0; font-weight: bold; color: rgb(21 94 117); width:350px;" >
             <p>Monto Total</p>
             <p>$${total}.00</p>
           </div>
