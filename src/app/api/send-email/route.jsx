86395b315aa6f0dd4ec387dc301cbb242c0394af
josesuchmolin <suchmolin11@gmail.com>
2024-08-16 13:35:07 -0400

    correccion de imagenes de email y formato html

--- src/app/api/send-email/route.jsx
+++ src/app/api/send-email/route.jsx
@@ -1,25 +1,31 @@
-import { Resend } from "resend";
-import { NextResponse } from "next/server";
+import { Resend } from "resend"
+import { NextResponse } from "next/server"
 
-const resend = new Resend("re_QpfqvXFg_7FY6LdA53EXGM5T22kSoFyCE");
+const resend = new Resend("re_QpfqvXFg_7FY6LdA53EXGM5T22kSoFyCE")
 
 export async function POST(req, res) {
-  const data = await req.json();
-  console.log(data);
+  const data = await req.json()
 
   const respuesta = await resend.emails.send({
     from: "no-reply@fyrlois.us",
     to: "info@fyrlois.us",
     subject: "CONTACT-US from Fyr Lois Academy Online",
     html: `
+    <html>
+              <head>
+              </head>
+              <body>
         <h1>Hola, mi nombre es: ${data.fullname}</h1>
         <p>Correo electrónico: ${data.email}</p>
         <p>Numero de telefono: ${data.tellphone}</p>
         <p>Motivo de aprender ingles: ${data.porqueAprender}</p>
         <p>Como nos conociste: ${data.comoNosConociste}</p>
         <p>Has estudiado ingles antes: ${data.hasEstudiadoAntes}</p>
+        </body>
+        </html>
+        
         `,
-  });
+  })
 
-  return NextResponse.json({ data: respuesta, message: "Email sent" });
+  return NextResponse.json({ data: respuesta, message: "Email sent" })
 }
--- src/app/api/webhook/route.jsx
+++ src/app/api/webhook/route.jsx
@@ -1,52 +1,55 @@
-import { NextResponse } from "next/server";
-import Stripe from "stripe";
-import { headers } from "next/headers";
-import { Resend } from "resend";
-import { cursosInfo } from "@/cursosInfo";
+import { NextResponse } from "next/server"
+import Stripe from "stripe"
+import { headers } from "next/headers"
+import { Resend } from "resend"
+import { cursosInfo } from "@/cursosInfo"
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-const resend = new Resend("re_QpfqvXFg_7FY6LdA53EXGM5T22kSoFyCE");
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+const resend = new Resend("re_QpfqvXFg_7FY6LdA53EXGM5T22kSoFyCE")
 
 export async function POST(request) {
-  const body = await request.text();
-  const headersList = headers();
-  const sig = headersList.get("stripe-signature");
+  const body = await request.text()
+  const headersList = headers()
+  const sig = headersList.get("stripe-signature")
 
-  let event;
+  let event
 
   try {
     event = stripe.webhooks.constructEvent(
       body,
       sig,
       process.env.STRIPE_WEBHOOK_SECRET
-    );
+    )
   } catch (error) {
-    console.console.log(error.message);
-    return NextResponse.error({ error: error.message });
+    console.console.log(error.message)
+    return NextResponse.error({ error: error.message })
   }
 
   switch (event.type) {
     case "checkout.session.completed":
-      const session = event.data.object;
-      const productos = JSON.parse(session.metadata.data);
-      const nombreCliente = session.customer_details.name;
-      const correoCliente = session.customer_details.email;
-      const total = session.amount_total / 100;
-      const idTransaccion = session.payment_intent;
-      const date = JSON.parse(event.created);
+      const session = event.data.object
+      const productos = JSON.parse(session.metadata.data)
+      const nombreCliente = session.customer_details.name
+      const correoCliente = session.customer_details.email
+      const total = session.amount_total / 100
+      const idTransaccion = session.payment_intent
+      const date = JSON.parse(event.created)
       const fecha = new Date(date * 1000).toLocaleDateString("es-ES", {
         timeZone: "America/New_York",
         hour: "2-digit",
         minute: "2-digit",
-      });
-      const telefono = session.custom_fields[0].numeric.value;
+      })
+      const telefono = session.custom_fields[0].numeric.value
 
       const respuesta = await resend.emails.send({
         from: "info@fyrlois.us",
         to: correoCliente,
         subject: "PAGO ONLINE Fyr Lois Online",
         html: `
-
+            <html>
+              <head>
+              </head>
+              <body>
               <h1>Hola, ${nombreCliente}</h1>
                 <p>se ha registrado un pago con identificador: ${idTransaccion}</p>
                 <p>En un lapso de máximo de 48 horas, serás contactado al correo: ${correoCliente} por nuestro equipo de Atención.</p>
@@ -54,7 +57,7 @@ export async function POST(request) {
 
 
           <div style="width: 450px; font-size: 12px; line-height: 16px; margin-left: 8px; border-radius: 6px; overflow: hidden;">
-        <img style="width: 100%; height: auto;" src="https://fyrlois-us.vercel.app/_next/image?url=%2Fimg%2FheadEmail.png&w=1920&q=75" alt="" />
+        <img style="width: 100%; height: auto;" src="https://fyrlois.us/_next/image?url=%2Fimg%2FheadEmail.png&w=1920&q=75" alt="" />
         <h2 style="text-align: center;">
           Numero de transaccion: ${idTransaccion}
         </h2>
@@ -77,13 +80,13 @@ export async function POST(request) {
 
         <div style="width: 100%; background-color: rgb(241 245 249); font-size: 14px; line-height: 16px; padding: 12px 16px; margin-top: 12px;">
           ${productos.map((producto) => {
-            const curso = cursosInfo.find((curso) => curso.id === producto.id);
+            const curso = cursosInfo.find((curso) => curso.id === producto.id)
             return `
               <div style="display: flex; gap: 16px; align-items: center; padding-top: 16px; padding-bottom: 16px;" >
                 <img
                 style="width: 50px; height: 50px; margin-right: 10px;"
                   
-                  src="https://fyrlois-us.vercel.app/_next/image?url=%2Fimg%2F${curso.img}&w=1920&q=75"
+                  src="https://fyrlois.us/_next/image?url=%2Fimg%2F${curso.img}&w=1920&q=75"
                   alt="${curso.title}"
                 />
                 <div style="width: 350px; display:flex;">
@@ -91,7 +94,7 @@ export async function POST(request) {
                   <p style="display:inline-block; margin-left: auto; margin-right: 0;">$${curso.price}.00</p>
                 </div>
               </div>
-              `;
+              `
           })}
           
           <hr />
@@ -103,14 +106,20 @@ export async function POST(request) {
           <p style="color: rgb(107 114 128);">Correo Electrónico : ${correoCliente}</p>
         </div>
         <p>Para más información, puedes comunicarte con nosotros, vía WhatsApp, por el número telefónico: +1 (786) 626-6559</p>
-      </div>`,
-      });
+      </div>
+      </body>
+      </html>
+      `,
+      })
       const respuesta2 = await resend.emails.send({
         from: "no-reply@fyrlois.us",
         to: "info@fyrlois.us",
         subject: "PAGO ONLINE Fyr Lois Online",
         html: `
-
+              <html>
+              <head>
+              </head>
+              <body>
               <h1>Hola, ${nombreCliente}</h1>
                 <p>se ha registrado un pago con identificador: ${idTransaccion}</p>
                 <p>En un lapso de máximo de 48 horas, serás contactado al correo: ${correoCliente} por nuestro equipo de Atención.</p>
@@ -118,7 +127,7 @@ export async function POST(request) {
 
 
           <div style="width: 450px; font-size: 12px; line-height: 16px; margin-left: 8px; border-radius: 6px; overflow: hidden;">
-        <img style="width: 100%; height: auto;" src="https://fyrlois-us.vercel.app/_next/image?url=%2Fimg%2FheadEmail.png&w=1920&q=75" alt="" />
+        <img style="width: 100%; height: auto;" src="https://fyrlois.us/_next/image?url=%2Fimg%2FheadEmail.png&w=1920&q=75" alt="" />
         <h2 style="text-align: center;">
           Numero de transaccion: ${idTransaccion}
         </h2>
@@ -141,13 +150,13 @@ export async function POST(request) {
 
         <div style="width: 100%; background-color: rgb(241 245 249); font-size: 14px; line-height: 16px; padding: 12px 16px; margin-top: 12px;">
           ${productos.map((producto) => {
-            const curso = cursosInfo.find((curso) => curso.id === producto.id);
+            const curso = cursosInfo.find((curso) => curso.id === producto.id)
             return `
               <div style="display: flex; gap: 16px; align-items: center; padding-top: 16px; padding-bottom: 16px;" >
                 <img
                 style="width: 50px; height: 50px; margin-right: 10px;"
                   
-                  src="https://fyrlois-us.vercel.app/_next/image?url=%2Fimg%2F${curso.img}&w=1920&q=75"
+                  src="https://fyrlois.us/_next/image?url=%2Fimg%2F${curso.img}&w=1920&q=75"
                   alt="${curso.title}"
                 />
                 <div style="width: 350px; display:flex;">
@@ -155,7 +164,7 @@ export async function POST(request) {
                   <p style="display:inline-block; margin-left: auto; margin-right: 0;">$${curso.price}.00</p>
                 </div>
               </div>
-              `;
+              `
           })}
           
           <hr />
@@ -167,13 +176,15 @@ export async function POST(request) {
           <p style="color: rgb(107 114 128);">Correo Electrónico : ${correoCliente}</p>
         </div>
         <p>Para más información, puedes comunicarte con nosotros, vía WhatsApp, por el número telefónico: +1 (786) 626-6559</p>
-      </div>`,
-      });
+      </div>
+      </body>
+      </html>`,
+      })
 
-      break;
+      break
     default:
-      console.log(`Unhandled event type ${event.type}`);
+      console.log(`Unhandled event type ${event.type}`)
   }
 
-  return new Response(null, { status: 200 });
+  return new Response(null, { status: 200 })
 }
